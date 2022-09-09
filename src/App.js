4b7c176aa4ebaa7ccff7272a443ebb317f6de9b2
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-09-09 20:52:22 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+       <h1>hello this react app for deploying</h1>
         </p>
         <a
           className="App-link"
