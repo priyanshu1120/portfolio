42bafefac39eff5b8b85c56ad6dfae408f2d8b3a
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-09-09 20:56:01 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      
        <h1>hello this react app for deploying</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello everyone this is the new react project
-        </a>
+       
       </header>
     </div>
   );
